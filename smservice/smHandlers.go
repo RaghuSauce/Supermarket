10766bb66c9dcb761801cfd47c85a16d4b5c456e
RaghuSauce <Dantuluri.Raghu@gmail.com>
2018-08-12 14:33:03 -0400

    updated http status codes for more accuracy

--- smservice/smHandlers.go
+++ smservice/smHandlers.go
@@ -13,14 +13,18 @@ import (
 	"os"
 )
 
+const(
+	BODYSIZELIMT = 1048576		// max payload size for posts
+)
+
 //Get Mapping	"/"
 func Index(w http.ResponseWriter, r *http.Request) {
 	file, err := ioutil.ReadFile("VERSION") // read the version file
 	if err != nil {                         //if an error occurs tell the user
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, err)
 	} else { // else return the version of the api in use
-		w.WriteHeader(200)
+		w.WriteHeader(http.StatusAccepted)
 		fmt.Fprintf(w, "%s%s", "Supermarket-API:", string(file))
 	}
 }
@@ -30,9 +34,11 @@ func GetOne(w http.ResponseWriter, r *http.Request) {
 	fileExits, item := smdb.GetProduceItem(produceCode) //see if it exits
 
 	if fileExits { //if it does return it
-		w.WriteHeader(200)
+		w.WriteHeader(http.StatusAccepted)
+		w.Header().Set("Content-Type", "application/json ; charset=UTF-8")
 		json.NewEncoder(w).Encode(item)
 	} else { // else let the user know it wasn't found
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "produce item with code %s not found in the database", produceCode)
 	}
 }
@@ -40,9 +46,10 @@ func GetOne(w http.ResponseWriter, r *http.Request) {
 //Get Mapping  "/fetch "
 func FetchProduceList(w http.ResponseWriter, r *http.Request) {
 	c := make(chan []smdb.ProduceItem)                                 //make for the list of produce items
+	defer close(c)
 	go smdb.ListProduceItems(c)                                        //populate the channel of the items
 	db := <-c                                                          //get the items in the channel
-	w.WriteHeader(200)                                                 //set the response code
+	w.WriteHeader(http.StatusAccepted)                                                 //set the response code
 	w.Header().Set("Content-Type", "application/json ; charset=UTF-8") //Set the response type
 	json.NewEncoder(w).Encode(db)                                      //return the list
 }
@@ -51,10 +58,12 @@ func FetchProduceList(w http.ResponseWriter, r *http.Request) {
 func GetLogs(w http.ResponseWriter, r *http.Request) {
 	b, err := ioutil.ReadFile("rest.log") // just pass the file name
 	if err != nil {                       //if there is an error return to the user and set header to 500
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, err)
 	}
 	str := string(b)     // convert content to a 'string'
+	w.Header().Set("Content-Type", "application/json ; charset=UTF-8") //Set the response type
+	w.WriteHeader(http.StatusAccepted)
 	fmt.Fprintln(w, str) // print the content as a 'string'
 }
 
@@ -63,22 +72,24 @@ func AddProduceItem(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "Add, %q", html.EscapeString(r.URL.Path))
 	var produce smdb.ProduceItem // Declare a produce Item to to unmarshal into
 
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576)) // Read the body of the request and limit the body size to 1MB
+	//w.Header().Set("Content-Type", "application/json ; charset=UTF-8") //Set the response type
+	w.WriteHeader(http.StatusInternalServerError)                          //Set the response Code
+
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, BODYSIZELIMT)) // Read the body of the request and limit the body size to 1MB
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(w,err)
 	}
 	if json.Unmarshal(body, &produce); err != nil { //Unmarshal the request into the struct, panic if an error occurs
-		w.Header().Set("Content-Type", "application/json ; charset=UTF-8") //Set the response type
-		w.WriteHeader(422)                                                 //Set the response Code
 
 		if err := json.NewEncoder(w).Encode(err); err != nil {
-			panic(err)
+			fmt.Fprintln(w,err)
 		}
 	}
 
 	if isValid, errs := smdb.ValidateProduceItem(produce); err == nil && isValid {
 		if e := smdb.AddProduceItemToDatabase(produce); e == nil {
-			w.WriteHeader(200)
+			w.WriteHeader(http.StatusAccepted)		//Set the response Code
+			w.Header().Set("Content-Type", "application/json ; charset=UTF-8") //Set the response type
 			fmt.Fprint(w, "Success")
 		} else {
 			fmt.Fprint(w, e)
@@ -88,7 +99,7 @@ func AddProduceItem(w http.ResponseWriter, r *http.Request) {
 		for _, err := range errs {
 			errorString += err.Error() + "\n"
 		}
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, "Produce Item is invalid for the following reasons \n\n", errorString)
 	}
 }
@@ -98,10 +109,10 @@ func AddProduceItem(w http.ResponseWriter, r *http.Request) {
 func RemoveProduceItem(w http.ResponseWriter, r *http.Request) {
 	produceCode := getProduceCodeUrlParameter(r)
 	if err := smdb.RemoveProduceItemFromDatabase(produceCode); err != nil {
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, err)
 	} else {
-		w.WriteHeader(200)
+		w.WriteHeader(http.StatusAccepted)
 		fmt.Fprint(w, "Success")
 	}
 }
@@ -110,10 +121,10 @@ func RemoveProduceItem(w http.ResponseWriter, r *http.Request) {
 //Wipe the logs out
 func CleanLogs(w http.ResponseWriter, r *http.Request) {
 	if err := os.Remove("rest.log"); err != nil { //remove the file containing the logs
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, err)
 	} else {
-		w.WriteHeader(200)
+		w.WriteHeader(http.StatusAccepted)
 		fmt.Fprint(w, "Logs Purged")
 	}
 }
